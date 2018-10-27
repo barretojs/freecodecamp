19c738bc678b74cd2a60a82fd31b92b13d00219b
Bruno Barreto Carvalho <barreto.carvalho.b@gmail.com>
2018-10-27 11:57:05 -0300

    root path on the html

--- file-metadata/src/main.ts
+++ file-metadata/src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
-import * as multer from 'multer';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
